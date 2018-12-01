9dbf23579602b362466146cbef0eb063b264092c
Dustin Schau <dustinschau@gmail.com>
2018-11-30 21:39:06 -0500

    chore: fix build

--- client/gatsby-browser.js
+++ client/gatsby-browser.js
@@ -1,13 +1,9 @@
 import React from 'react'
 import { ApolloProvider } from 'react-apollo'
-import ApolloClient from 'apollo-boost'
+import { client } from './src/client'
 
 import 'normalize.css'
 import 'typeface-montserrat'
-
-const client = new ApolloClient({
-  uri: `TODO`,
-})
 
 export const wrapRootElement = ({ element }) => {
   return <ApolloProvider client={client}>{element}</ApolloProvider>
--- client/gatsby-ssr.js
+++ client/gatsby-ssr.js
@@ -1,7 +1,7 @@
-/**
- * Implement Gatsby's SSR (Server Side Rendering) APIs in this file.
- *
- * See: https://www.gatsbyjs.org/docs/ssr-apis/
- */
+import React from 'react'
+import { ApolloProvider } from 'react-apollo'
+import { client } from './src/client'
 
-// You can delete this file if you're not using it
+export const wrapRootElement = ({ element }) => (
+  <ApolloProvider client={client}>{element}</ApolloProvider>
+)
