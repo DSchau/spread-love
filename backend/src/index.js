fc7e05e57bb005c2d58b2109a40ddbd65501c55a
Dustin Schau <dustinschau@gmail.com>
2018-12-01 01:14:06 -0500

    chore: add cors

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,7 +1,8 @@
+const cors = require('micro-cors')();
 const { ApolloServer } = require('apollo-server-micro');
 
 const schema = require('./schema');
 
 const server = new ApolloServer(schema);
 
-module.exports = server.createHandler();
+module.exports = cors(server.createHandler());
