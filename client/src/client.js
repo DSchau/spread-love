6024ceb132522f80a0fe16e0c7b5d09275b469aa
Dustin Schau <dustinschau@gmail.com>
2019-01-14 06:01:16 +0100

    chore: update client

--- client/src/client.js
+++ client/src/client.js
@@ -2,6 +2,6 @@ import ApolloClient from 'apollo-boost'
 import fetch from 'isomorphic-fetch'
 
 export const client = new ApolloClient({
-  uri: `https://backend-a88p4d1h3.now.sh/graphql`,
+  uri: `https://backend-4e442b0jk.now.sh/graphql`,
   fetch,
 })
